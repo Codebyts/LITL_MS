f1b5e83ca4b3e764791d8e657cf5e2ef3474a01e
Codebyts <118887161+Codebyts@users.noreply.github.com>
2025-02-02 22:52:33 +0800

    Delete validation.php

--- validation.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	
-	if ($_SESSION['status'] == 'invalid' || empty($_SESSION['status'])) {
-        $_SESSION['status'] = 'invalid'; 
-		unset($_SESSION['officer_id']);
-		header("Location: login-page.php");
-	}	
-?>
